3d7881e354ddb5530e51946399ee8ef0599c0475
Rit0ast <juanf64b@gmail.com>
2026-09-09 10:27:23 -0700

    Added helper function to run processes

--- src/Core/ToolResolver.cs
+++ src/Core/ToolResolver.cs
@@ -2,13 +2,20 @@ using System;
 
 namespace YtDlpForMusicBee.Core
 {
+    public class RunProcessResult
+    {
+        public bool TimedOut { get; set; }
+        public int ExitCode { get; set; }
+        public string StandardOutput { get; set; }
+        public string StandardError { get; set; }
+    }
     public class ToolResolver
     {
+        private const int TimeoutMilliseconds = 100;
         public string YtDlpPath { get; private set; }
         public string FfmpegPath { get; private set; }
-
-        public bool YtDlpAvailable;
-        public bool FfmpegAvailable;
+        public bool YtDlpAvailable { get; private set; }
+        public bool FfmpegAvailable { get; private set; }
 
         // Resolve the path for both YtDlp and Ffmpeg, then verify that they can be run.
         public void Resolve()
@@ -26,6 +33,13 @@ namespace YtDlpForMusicBee.Core
 
         // Verify that the tool runs by calling `-h` on it.
         private bool VerifyRuns(string path)
+        {
+            // TODO: implement
+            throw new NotImplementedException();
+        }
+        
+        // Helper to run processes. Used by ResolveTool and VerifyRuns
+        private RunProcessResult RunProcess(string fileName, string arguments, TimeSpan timeout)
         {
             // TODO: implement
             throw new NotImplementedException();
