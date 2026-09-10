e858eb577f119b29021782416a0490ca624fe451
Rit0ast <juanf64b@gmail.com>
2026-09-10 09:07:48 -0700

    Create UI skeleton and add it to MusicBee's tools menu

---
+++ src/UI/PluginForm.cs
@@ -0,0 +1,38 @@
+using System.Windows.Forms;
+
+namespace YtDlpForMusicBee.UI
+{
+    /// PluginForm, only one instance of this is created
+    public class PluginForm : Form
+    {
+        private PluginControl _pluginControl;
+
+        public PluginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            _pluginControl = new PluginControl { Dock = DockStyle.Fill };
+
+            Text = "yt-dlp for MusicBee";
+            ClientSize = _pluginControl.Size;
+            FormBorderStyle = FormBorderStyle.Sizable;
+            MaximizeBox = false;
+            MinimizeBox = true;
+            StartPosition = FormStartPosition.CenterParent;
+
+            Controls.Add(_pluginControl);
+
+            FormClosing += PluginForm_FormClosing;
+        }
+
+        private void PluginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Keep the instance alive, only hide/show when necessary
+            e.Cancel = true;
+            Hide();
+        }
+    }
+}
